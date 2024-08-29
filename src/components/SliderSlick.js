b4f22df3f14e975e90a19f18823cfa9556ec382f
Sajeed <syedsajeed6969@gmail.com>
2024-08-29 09:05:57 +0530

    auto sroll, responsive, custum component

--- src/components/SliderSlick.js
+++ src/components/SliderSlick.js
@@ -1,5 +1,28 @@
 import React, { useRef } from "react";
 import Slider from "react-slick";
+import '../App.css'
+
+
+function CustomSlide(props) {
+    const { index, ...otherProps } = props;
+    return (
+        <div {...otherProps}>
+            <div class="card testimonial-card mt-2 mb-3 mx-2">
+                <div class="card-up aqua-gradient"></div>
+                <div class="avatar mx-auto white">
+                    <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2831%29.jpg" class="rounded-circle img-fluid"
+                        alt="woman avatar" />
+                </div>
+                <div class="card-body text-center">
+                    <h4 class="card-title font-weight-bold">Martha Smith</h4>
+                    <hr />
+                    <p><i class="fas fa-quote-left"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos,
+                        adipisci</p>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 function AutoPlayMethods() {
     let sliderRef = useRef(null);
@@ -15,32 +38,48 @@ function AutoPlayMethods() {
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000
+        // autoplay: true,
+        // autoplaySpeed: 2000
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+
     };
     return (
         <div className="slider-container">
             <h2>Auto Play {"&"} Pause with buttons</h2>
             <Slider ref={slider => (sliderRef = slider)} {...settings}>
-                <div className="card mx-2">
-                    <h3>1</h3>
-                </div>
-                <div className="card mx-2">
-                    <h3>2</h3>
-                </div>
-                <div className="card mx-2">
-                    <h3>3</h3>
-                </div>
-                <div className="card mx-2">
-                    <h3>4</h3>
-                </div>
-                <div className="card mx-2">
-                    <h3>5</h3>
-                </div>
-                <div className="card mx-2">
-                    <h3>6</h3>
-                </div>
+                <CustomSlide index={1} />
+                <CustomSlide index={2} />
+                <CustomSlide index={3} />
+                <CustomSlide index={4} />
+                <CustomSlide index={5} />
+                <CustomSlide index={6} />
             </Slider>
+            <br /><br />
             <div style={{ textAlign: "center" }}>
                 <button className="button" onClick={play}>
                     Play
